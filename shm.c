7007a0678f49668671076e707a1c2ce92f140ab0
andrew munoz <amuno032@sledge.cs.ucr.edu>
2018-12-02 20:51:54 -0800

    Cleaned up code a bit and reorganized areas that might be causeing the problem. However, shm_open still only runs once before trap 14 error

--- shm.c
+++ shm.c
@@ -30,61 +30,73 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
 
-  uint caseNumber = 2;
   uint i;
+  uint exists = 0; 	        // used to see if page already is being shared
+  uint firstFreeAddress = 100;
+  uint va;	// Free virtual address to map page to
   uint indexPage; 
-  uint firstFreeAddress = 0;
-  uint va = PGROUNDUP(myproc()->sz);
   char* mem;
 
   // check if id already exists
   acquire(&(shm_table.lock));
-  for (i = 0; i< 64; i++) {  
-    if (shm_table.shm_pages[i].id == id) {
-      caseNumber = 1; 
-      indexPage = i;
-    }
-    if ((shm_table.shm_pages[i].id == 0) && (firstFreeAddress == 0)){
-	firstFreeAddress = i;
-      }
-  }
-/*
+
   // Search table to see if the id already exists
   for(i = 0; i < 64; i+=1){
     if (shm_table.shm_pages[i].id == id){
-      indexPage = i;
+      indexPage = i; 	// We found the page
+      exists = 1;
+    }
+    else{
+       if (shm_table.shm_pages[i].frame == 0 && firstFreeAddress == 100){ // This is a free table
+         firstFreeAddress = i;
+       } 
     }
   }
-*/
   release(&(shm_table.lock));
-  cprintf("meme\n");
 
-  // id we are opening already exists. map the page 
-  if (caseNumber == 1) {
+  // Case 1: Id for page exists
+  if (exists) {
     acquire(&(shm_table.lock));
+
+    va = PGROUNDUP(myproc()->sz);
     mappages(myproc()->pgdir, (char *)va, PGSIZE, 
 	     V2P(shm_table.shm_pages[indexPage].frame), PTE_W | PTE_U);
-    ++(shm_table.shm_pages[indexPage].refcnt);
+    ++(shm_table.shm_pages[indexPage].refcnt); // Increment refcnt
+    *pointer = (char *)va;  // Return pointer to the virtual adress
+    myproc()->sz = va;
+    myproc()->sz += PGSIZE;
+
     release(&(shm_table.lock));
+    return 0;
   }
   
-  // id we want to open does not exist
-  else if (caseNumber == 2) {
+  // Case 2: Id for page doesnt exist, first to shm_open
+  else {
     mem = kalloc();
+    cprintf("%d\n", mem);
     memset(mem, 0, PGSIZE);
     acquire(&(shm_table.lock));
+
+    va = PGROUNDUP(myproc()->sz);
     shm_table.shm_pages[firstFreeAddress].frame = mem;
-    mappages(myproc()->pgdir, (char *)va, PGSIZE, 
-	     V2P(shm_table.shm_pages[firstFreeAddress].frame), PTE_W | PTE_U);
-    ++(shm_table.shm_pages[firstFreeAddress].refcnt);
+    cprintf("%d\n", shm_table.shm_pages[firstFreeAddress].frame);
+    if(mappages(myproc()->pgdir, (char *)va, PGSIZE, 
+	     V2P(shm_table.shm_pages[firstFreeAddress].frame), PTE_W | PTE_U) < 0){
+	cprintf("allocuvm out of memory\n");
+    }
     shm_table.shm_pages[firstFreeAddress].id = id;
+
+    *pointer = (char *)va; // Return pointer to the virtual address
+    shm_table.shm_pages[firstFreeAddress].refcnt = 1; // Increment refcnt
+    myproc()->sz = va;
+    myproc()->sz += PGSIZE;
+
     release(&(shm_table.lock));
+    return 0;
   }
 
-  *pointer = (char *)va;
-  myproc()->sz = va;
-
-return 0; //added to remove compiler warning -- you should decide what to return
+  //myproc()->sz = va;
+  return -1; //added to remove compiler warning -- you should decide what to return
 }
 
 
